68783a3575013ac25d3557cd3a7c62ecf9aa2ae3
Emanuel <andradellos@hotmail.com>
2020-04-11 12:37:29 -0300

    Tab

--- stats.js
+++ stats.js
@@ -1,2 +1,22 @@
 const os = require ('os');
-console.log(os.platform());
+
+setInterval(() => {
+
+    const {freemem, totalmem } = os;
+
+    const total = parseInt(totalmem()/ 1024 /1024);
+    const mem = parseInt(freemem()/ 1024 /1024);
+    const percent =  parseInt((mem / total) * 100);
+    
+    const objStats = {
+        free : `${mem} MB`,
+        total :`${total} MB`,
+        usage : `${percent} %`
+    }
+    console.clear();
+    console.log('+++++++Bom++++++++');
+    console.table(objStats);
+    
+}, 1000)
+
+
