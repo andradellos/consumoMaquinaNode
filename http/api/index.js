5870e6312636bca4e457c2dd489797a771cd19f0
Emanuel <andradellos@hotmail.com>
2020-04-21 23:05:51 -0300

    Criação API

---
+++ http/api/index.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const URL = require('url');
+const fs = require('fs');
+const path = require('path');
+
+const dataJson = require('./urls.json');
+http.createServer((req, res) => {
+
+    function saveJson  (cb){
+
+        return fs.writeFile(
+            path.join(__dirname, 'urls.json'), 
+            JSON.stringify(dataJson,null,2),
+            err => {
+                if (err) throw err
+               cb(JSON.stringify({mensage:'OK'}))
+            })
+        }
+
+
+  const {name, url, del} =  URL.parse(req.url, true).query;
+
+    if (!name || !url)
+        return   res.end(JSON.stringify(dataJson))
+
+    if (del){
+        dataJson.urls = dataJson.urls.filter(item => String(item.url) !== String(url))
+       return saveJson((mensage) => res.end(mensage))
+    }else{       
+      dataJson.urls.push({name,url})
+      return saveJson((mensage) => res.end(mensage))
+    }
+ 
+}).listen(3000, () => {console.log('API  is running!!!')})
